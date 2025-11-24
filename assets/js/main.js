83d915a389e9cd58da4c02ac4b173fb8d76ea043
itsyouryaffa <edithzhang17@gmail.com>
2025-11-24 14:02:56 +0100

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.esm.js';
+import GameScene from './scenes/GameScene.js';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 480,
+  physics: {
+    default: 'arcade',
+    arcade: { gravity: { y: 1000 }, debug: false }
+  },
+  scene: [GameScene]
+};
+
+new Phaser.Game(config);
